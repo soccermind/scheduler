c8908554fd761750ca4e661e9234a48ac0d32ac9
soccermind <mty2yyz@gmail.com>
2020-12-08 17:37:29 -0500

    Added InterviewerList component with styles and stories

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+import InterviewerListItem from "./InterviewerListItem";
+
+import "./InterviewerList.scss";
+
+export default function InterviewerList(props) {
+  const parsedInterviewerList = props.interviewers && props.interviewers.map(interviewer => 
+    <InterviewerListItem 
+      key={interviewer.id} 
+      name={interviewer.name} 
+      avatar={interviewer.avatar}
+      selected={interviewer.id === props.interviewer}
+    />);
+
+  return (
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list">{parsedInterviewerList && parsedInterviewerList.length ? parsedInterviewerList : "This list is empty"}</ul>
+    </section>
+  );
+}
