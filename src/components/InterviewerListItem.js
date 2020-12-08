8c868a63bef978c2d483e00ca1ec4af681af9472
soccermind <mty2yyz@gmail.com>
2020-12-08 15:57:20 -0500

    Added InterviwerListItem component with styles and stories

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,30 @@
+import React from "react";
+import classNames from 'classnames';
+
+import "./InterviewerListItem.scss";
+
+export default function InterviewerListItem(props) {
+  let interviewersClass = classNames(
+    'interviewers__item', 
+      {
+      'interviewers__item--selected': props.selected
+      }
+    );
+  let interviewersClassImage = classNames(
+    'interviewers__item-image', 
+      {
+      'interviewers__item--selected-image': props.selected
+      }
+    );
+
+  return (
+    <li className={interviewersClass} onClick={() => props.setInterviewer(props.name)}>
+      <img
+        className={interviewersClassImage}
+        src={props.avatar}
+        alt={props.name}
+      />
+      {props.selected && props.name}
+    </li>
+  );
+}
