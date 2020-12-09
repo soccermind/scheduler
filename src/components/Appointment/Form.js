e139a841d443763df7a3367b826c44cd22086789
soccermind <mty2yyz@gmail.com>
2020-12-08 23:14:11 -0500

    Added Appointment component Form and stories

---
+++ src/components/Appointment/Form.js
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+
+import Button from "../Button";
+import InterviewerList from "../InterviewerList";
+
+
+export default function Form(props) {
+  const [name, setName] = useState(props.name || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
+  const onSave = event => {
+    props.onSave(name, interviewer);
+  };
+  const reset = () => {
+    setName("");
+    setInterviewer(null);
+  };
+  const cancel = () => {
+    reset();
+    props.onCancel();
+  };
+
+  return (
+    <main className="appointment__card appointment__card--create">
+      <section className="appointment__card-left">
+        <form autoComplete="off" onSubmit={event => event.preventDefault()}>
+          <input
+            className="appointment__create-input text--semi-bold"
+            name="name"
+            type="text"
+            placeholder="Enter Student Name"
+            value={name}
+            onChange={event => setName(event.target.value)}
+            /*
+              This must be a controlled component
+            */
+          />
+        </form>
+        <InterviewerList 
+          interviewers={props.interviewers} 
+          interviewer={interviewer} 
+          setInterviewer={setInterviewer} 
+        />
+      </section>
+      <section className="appointment__card-right">
+        <section className="appointment__actions">
+          <Button danger onClick={cancel}>Cancel</Button>
+          <Button confirm onClick={onSave}>Save</Button>
+        </section>
+      </section>
+    </main>
+  );
+}
