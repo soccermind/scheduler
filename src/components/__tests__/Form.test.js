fd667bf9e4026842e71c6c9a946707ec815ade3b
soccermind <mty2yyz@gmail.com>
2020-12-16 17:39:17 -0500

    Fixed Form test after adding validation for missing interviewer when booking appointment

--- src/components/__tests__/Form.test.js
+++ src/components/__tests__/Form.test.js
@@ -38,7 +38,7 @@ describe("Form", () => {
   
   it("can successfully save after trying to submit an empty student name", () => {
     const onSave = jest.fn();
-    const { getByText, getByPlaceholderText, queryByText } = render(
+    const { getByText, getByPlaceholderText, queryByText, getByAltText } = render(
       <Form interviewers={interviewers} onSave={onSave} />
     );
   
@@ -50,13 +50,14 @@ describe("Form", () => {
     fireEvent.change(getByPlaceholderText("Enter Student Name"), {
       target: { value: "Lydia Miller-Jones" }
     });
+    fireEvent.click(getByAltText("Sylvia Palmer"));
   
     fireEvent.click(getByText("Save"));
   
     expect(queryByText(/student name cannot be blank/i)).toBeNull();
   
     expect(onSave).toHaveBeenCalledTimes(1);
-    expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", null);
+    expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", 1);
   });
 
   it("calls onCancel and resets the input field", () => {
